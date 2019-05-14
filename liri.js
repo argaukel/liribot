4ed979a9482bcaa2e57122c17d793deab0c34bb2
arg <anna.gaukel@gmail.com>
2019-05-14 17:48:36 -0500

    response from bands in town

--- liri.js
+++ liri.js
@@ -1,3 +1,22 @@
 require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+var bandNameOne = process.argv[2];
+var bandNameTwo = process.argv[3];
+
+axios.get("https://rest.bandsintown.com/artists/" + bandNameOne + bandNameTwo + "/events?app_id=codingbootcamp").then(
+    function(response) {
+      console.log(response);
+    }
+  );
+    
+
+
+// function concertThis(bandNameOne, bandNameTwo) {
+//     console.log("https://rest.bandsintown.com/artists/" + bandNameOne + bandNameTwo + "/events?app_id=codingbootcamp");
+//     console.log(bandNameOne + bandNameTwo);
+// }
+
+// concertThis(bandNameOne, bandNameTwo)
