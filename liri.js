0397618076c90d325b4d04110372f23030dfeccd
arg <anna.gaukel@gmail.com>
2019-05-14 18:16:13 -0500

    spotify search working for multiple entries

--- liri.js
+++ liri.js
@@ -6,12 +6,20 @@ var keys = require("./keys.js");
 var bandNameOne = process.argv[2];
 var bandNameTwo = process.argv[3];
 
+
+if (bandNameTwo !== 'undefined' && bandNameTwo) {
 axios.get("https://rest.bandsintown.com/artists/" + bandNameOne + bandNameTwo + "/events?app_id=codingbootcamp").then(
     function(response) {
       console.log(response);
     }
   );
-    
+} else {
+    axios.get("https://rest.bandsintown.com/artists/" + bandNameOne + "/events?app_id=codingbootcamp").then(
+    function(response) {
+      console.log(response);
+    }
+    );
+}
 
 
 // function concertThis(bandNameOne, bandNameTwo) {
