ee7a6fe73204de2374927e5fbee59357ac3faccf
arg <anna.gaukel@gmail.com>
2019-05-21 21:19:50 -0500

    env problems

--- keys.js
+++ keys.js
@@ -1,12 +1,12 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: "3bc520d1729648d188cb57359286821b",
-  secret: "66032fddc808407c93c014fc70ac3e2a"
-  // redirectUri: 'myRedirectUri',
-};
-
 // exports.spotify = {
-//   id: process.env.SPOTIFY_ID,
-//   secret: process.env.SPOTIFY_SECRET
+//   id: "3bc520d1729648d188cb57359286821b",
+//   secret: "66032fddc808407c93c014fc70ac3e2a"
+//   // redirectUri: 'myRedirectUri',
 // };
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
